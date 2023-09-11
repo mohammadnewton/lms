1dffd038293b51aec7584f50553d7c80f24f217f
Newton Ombese <newtonombese1@gmail.com>
2023-09-11 06:20:35 +0300

    Setting up routes and cors

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+require('dotenv').config();
+
+import express, { NextFunction, Request, Response } from 'express';
 
 export const app = express();
 
@@ -13,5 +15,20 @@ app.use(cookieParser());
 
 // cors => cross origin resource sharing
 app.use(cors({
-  
-}))
+  origin: process.env.ORIGIN
+}));
+
+// testing api
+app.get('/test', (req:Request, res:Response, next:NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: 'success'
+  });
+});
+
+// unknown route
+app.all("*", (req:Request, res:Response, next:NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+})
