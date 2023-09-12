2e01a2ee9cadd57474c06cc8e9683325a3c187f8
Newton Ombese <newtonombese1@gmail.com>
2023-09-12 05:24:09 +0300

    Working on the user model

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -16,3 +16,44 @@ export interface IUser extends Document {
   courses: Array<{courseId: string}>;
   comparePassword: (password: string) => Promise<boolean>;
 }
+
+const userSchema: Schema<IUser> = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    validate: {
+      validator: function (value:string){
+        return emailRegexPattern.test(value);
+      },
+      message: 'Please enter a valid email',
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: [8, "Password must be at least 8 characters"],
+    select: false, 
+  },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  courses: [
+    {
+      courseId: String,
+    },
+  ],
+}, {timestamps: true}); 
