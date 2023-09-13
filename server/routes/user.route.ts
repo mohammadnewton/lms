14b8eeb33c07cc6df630ac8b9b7f82f33d2129c3
Newton Ombese <newtonombese1@gmail.com>
2023-09-13 07:19:34 +0300

    Add the activate user route

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { registerUser } from "../controllers/user.controller";
+import { activateUser, registerUser } from "../controllers/user.controller";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registerUser);
+
+userRouter.post("/activate-user", activateUser);
 
 export default userRouter;
