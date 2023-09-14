cd309af7304a296978205d0dccc54d8a30f72cd0
Newton Ombese <newtonombese1@gmail.com>
2023-09-14 07:57:18 +0300

    Implement user login and logout controllers

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -7,6 +7,7 @@ import jwt, {Secret} from 'jsonwebtoken';
 import ejs from 'ejs';
 import path from 'path';
 import sendMail from "../utils/sendMail";
+import { sendToken } from "../utils/jwt";
 
 // register user
 interface IRegistrationBody{
@@ -109,6 +110,54 @@ export const activateUser = CatchAsyncError(async(req:Request, res:Response, nex
 
     res.status(201).json({
       success: true
+    })
+  } catch (error:any) {
+    return next(new ErrorHandler(error.message, 400))
+  }
+})
+
+// Login user
+interface ILoginRequest {
+  email: string;
+  password: string;
+}
+
+export const loginUser = CatchAsyncError(async(req:Request, res:Response, next:NextFunction) => {
+  try {
+    const { email, password } = req.body as ILoginRequest;
+
+    if(!email || !password) {
+      return next(new ErrorHandler('Please enter email and password', 400))
+    }
+
+    const user = await userModel.findOne({ email }).select("+password");
+    if (!user) {
+      return next(new ErrorHandler("Invalid email or password", 404));
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+    if (!isPasswordMatched) {
+      return next(new ErrorHandler("Invalid email or password", 400));
+    }
+
+    sendToken(user, 200, res);
+    // const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as Secret);
+    // res.status(200).json({
+    //   success: true,
+    //   token
+    // })
+  } catch (error:any) {
+    return next(new ErrorHandler(error.message, 400))
+  }
+})
+
+// logout user
+export const logoutUser = CatchAsyncError(async(req:Request, res:Response, next:NextFunction) => {
+  try {
+    res.cookie("access_token", "", {maxAge: 1});
+    res.cookie("refresh_token", "", {maxAge: 1});
+    res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
     })
   } catch (error:any) {
     return next(new ErrorHandler(error.message, 400))
