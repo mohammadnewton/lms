44d99ed0262809bb3cbfd5cabbf66da637483642
Newton Ombese <newtonombese1@gmail.com>
2023-09-14 07:57:30 +0300

    Add login and logout routes

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { activateUser, registerUser } from "../controllers/user.controller";
+import { activateUser, loginUser, logoutUser, registerUser } from "../controllers/user.controller";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registerUser);
 
 userRouter.post("/activate-user", activateUser);
+
+userRouter.post('/login', loginUser);
+
+userRouter.post('/logout', logoutUser);
 
 export default userRouter;
