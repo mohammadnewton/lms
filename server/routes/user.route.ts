07eac5af48272c4fb0ebd8f8e390854fdf14b4c1
Newton Ombese <newtonombese1@gmail.com>
2023-09-15 09:02:15 +0300

    Add an auth check on the logout route

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { activateUser, loginUser, logoutUser, registerUser } from "../controllers/user.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registerUser);
@@ -8,6 +9,6 @@ userRouter.post("/activate-user", activateUser);
 
 userRouter.post('/login', loginUser);
 
-userRouter.post('/logout', logoutUser);
+userRouter.get('/logout', isAuthenticated, logoutUser);
 
 export default userRouter;
