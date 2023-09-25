bbd59ae9c8ba8864f2f8631eaa010a368ebc7f53
Newton Ombese <newtonombese1@gmail.com>
2023-09-25 15:54:25 +0300

    Working on the updating of user info

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -195,6 +195,8 @@ export const updateAccessToken = CatchAsyncError(async(req:Request,res:Response,
       expiresIn: "3d",
     });
 
+    req.user = user;
+
     res.cookie("access_token", accessToken, accessTokenOptions)
     res.cookie("refresh_token", refreshToken, refreshTokenOptions)
 
@@ -238,3 +240,40 @@ export const socialAuth = CatchAsyncError(async(req:Request, res:Response, next:
     return next(new ErrorHandler(error.message, 400));
   }
 });
+
+// update user info
+
+interface IUpdateUserInfo {
+  name?: string;
+  email?: string;
+}
+
+export const updateUserInfo = CatchAsyncError(async(req:Request, res:Response, next:NextFunction) => {
+  try {
+    const {name, email} = req.body as IUpdateUserInfo;
+    const userId = req.user?._id;
+    const user = await userModel.findById(userId);
+
+    if(email && user){
+      const isEmailExist = await userModel.findOne({email});
+      if(isEmailExist){
+        return next(new ErrorHandler('Email already exist', 400));
+      }
+      user.email = email;
+    }
+
+    if (name && user){
+      user.name = name;
+    }
+
+    await user?.save();
+
+    await redis.set(userId, JSON.stringify(user));
+    res.status(201).json({
+      success: true,
+      user,
+    })
+  } catch (error:any) {
+    return next(new ErrorHandler(error.message, 400));
+  }
+})
