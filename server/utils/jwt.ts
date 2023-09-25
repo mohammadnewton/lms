46a75eadc785411f3fff362f03a328ed5210749a
Newton Ombese <newtonombese1@gmail.com>
2023-09-25 07:28:10 +0300

    WOrking on the update Access Token

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 import { Response } from "express";
 import { IUser } from "../models/user.model";
 import { redis } from "./redis";
@@ -7,38 +7,44 @@ interface ITokenOptions {
   expires: Date;
   maxAge: number;
   httpOnly: boolean;
-  sameSite: 'lax' | 'strict' | 'none' | undefined;
+  sameSite: "lax" | "strict" | "none" | undefined;
   secure?: boolean;
 }
 
-export const sendToken = (user:IUser, statusCode: number, res:Response) => {
+// parse environment variables to integrate with fallback values
+const accessTokenExpire = parseInt(
+  process.env.ACCESS_TOKEN_EXPIRE || "300",
+  10
+);
+const refreshTokenExpire = parseInt(
+  process.env.REFRESH_TOKEN_EXPIRE || "1200",
+  10
+);
+
+// options for cookies
+export const accessTokenOptions: ITokenOptions = {
+  expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+  maxAge: accessTokenExpire * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
+
+export const refreshTokenOptions: ITokenOptions = {
+  expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 1000),
+  maxAge: refreshTokenExpire * 24 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
+
+export const sendToken = (user: IUser, statusCode: number, res: Response) => {
   const accessToken = user.SignAccessToken();
   const refreshToken = user.SignRefreshToken();
 
   // upload session
   redis.set(user._id, JSON.stringify(user) as any);
 
-  // parse environment variables to integrate with fallback values
-  const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || '300', 10);
-  const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || '1200', 10);
-
-  // options for cookies
-  const accessTokenOptions: ITokenOptions = {
-    expires: new Date(Date.now() + accessTokenExpire * 1000),
-    maxAge: accessTokenExpire * 1000,
-    httpOnly: true,
-    sameSite: 'lax',
-  }
-
-  const refreshTokenOptions: ITokenOptions = {
-    expires: new Date(Date.now() + refreshTokenExpire * 1000),
-    maxAge: refreshTokenExpire * 1000,
-    httpOnly: true,
-    sameSite: 'lax',
-  }
-
   // only set secure to true in production
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     accessTokenOptions.secure = true;
     // refreshTokenOptions.secure = true;
   }
@@ -50,5 +56,5 @@ export const sendToken = (user:IUser, statusCode: number, res:Response) => {
     success: true,
     user,
     accessToken,
-  })
-}
+  });
+};
